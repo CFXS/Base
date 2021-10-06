e43406ede86c58477012fc8b006d14efe2547401
RVeips <rihardsveips@gmail.com>
2021-10-07 02:32:05 +0300

    volatile time

--- include/CFXS/Base/Time.hpp
+++ include/CFXS/Base/Time.hpp
@@ -5,8 +5,8 @@ namespace CFXS {
     using Time_t = uint64_t;
 
     namespace Time {
-        extern Time_t ms;
-        extern Time_t cycles;
+        extern volatile Time_t ms;
+        extern volatile Time_t cycles;
     }; // namespace Time
 
 } // namespace CFXS
