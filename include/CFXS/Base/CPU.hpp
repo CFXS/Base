fad726cf27fe7570f35518fcfd22595f3abe9d81
RVeips <rihardsveips@gmail.com>
2022-01-31 00:03:26 +0200

    ref

--- include/CFXS/Base/CPU.hpp
+++ include/CFXS/Base/CPU.hpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS Framework Base Module <https://github.com/CFXS/CFXS-Base>
 // Copyright (C) 2022 | CFXS / Rihards Veips
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -26,9 +26,15 @@ namespace CFXS::CPU {
     static constexpr size_t CYCLES_PER_USEC = CLOCK_FREQUENCY / 1000000;
 
     // TODO: platform specific template
-    static __always_inline uint32_t GetCycleCount() { return (*(volatile uint32_t*)0xE0001004); }
-    static __always_inline void EnableInterrupts() { asm volatile("cpsie i"); };
-    static __always_inline void DisableInterrupts() { asm volatile("cpsid i"); };
+    static __always_inline uint32_t GetCycleCount() {
+        return (*(volatile uint32_t*)0xE0001004);
+    }
+    static __always_inline void EnableInterrupts() {
+        asm volatile("cpsie i");
+    };
+    static __always_inline void DisableInterrupts() {
+        asm volatile("cpsid i");
+    };
     static __always_inline bool AreInterruptsEnabled() {
         uint32_t primask;
         asm volatile("mrs %0, primask" : "=r"(primask));
