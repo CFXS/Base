b7364e1eccd762c43e34c4a8291c69d81d76960f
RVeips <rihardsveips@gmail.com>
2022-02-02 00:46:41 +0200

    refac

--- include/CFXS/Base/Logger.hpp
+++ include/CFXS/Base/Logger.hpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS Framework Base Module <https://github.com/CFXS/CFXS-Base>
 // Copyright (C) 2022 | CFXS / Rihards Veips
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -22,13 +22,13 @@
 
 namespace CFXS {
 
-    template<typename CFG>
+    template<typename Config>
     class Logger {
     public:
         template<typename... Args>
         static void Log(Args&&... args) {
-            if constexpr (CFG::ENABLED) {
-                printf(CFG::LOG_PREFIX);
+            if constexpr (Config::ENABLED) {
+                printf(Config::LOG_PREFIX);
                 printf(std::forward<Args>(args)...);
                 printf("\n");
             }
@@ -36,8 +36,8 @@ namespace CFXS {
 
         template<typename... Args>
         static void LogConstructor(Args&&... args) {
-            if constexpr (CFG::ENABLED) {
-                printf(CFG::CONSTRUCTOR_PREFIX);
+            if constexpr (Config::ENABLED) {
+                printf(Config::CONSTRUCTOR_PREFIX);
                 printf(std::forward<Args>(args)...);
                 printf("\n");
             }
@@ -45,8 +45,8 @@ namespace CFXS {
 
         template<typename... Args>
         static void LogDestructor(Args&&... args) {
-            if constexpr (CFG::ENABLED) {
-                printf(CFG::DESTRUCTOR_PREFIX);
+            if constexpr (Config::ENABLED) {
+                printf(Config::DESTRUCTOR_PREFIX);
                 printf(std::forward<Args>(args)...);
                 printf("\n");
             }
@@ -54,8 +54,8 @@ namespace CFXS {
 
         template<typename... Args>
         static void LogError(Args&&... args) {
-            if constexpr (CFG::ENABLED) {
-                printf(CFG::ERROR_PREFIX);
+            if constexpr (Config::ENABLED) {
+                printf(Config::ERROR_PREFIX);
                 printf(std::forward<Args>(args)...);
                 printf("\n");
             }
@@ -63,8 +63,8 @@ namespace CFXS {
 
         template<typename... Args>
         static void LogWarning(Args&&... args) {
-            if constexpr (CFG::ENABLED) {
-                printf(CFG::WARNING_PREFIX);
+            if constexpr (Config::ENABLED) {
+                printf(Config::WARNING_PREFIX);
                 printf(std::forward<Args>(args)...);
                 printf("\n");
             }
