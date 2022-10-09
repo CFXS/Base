6254cc396e1a819d9e038c2876ea6ee793e5d1d8
RVeips <rihardsveips@gmail.com>
2022-10-09 20:20:50 +0300

    enum overload

--- include/CFXS/Base/Utility.hpp
+++ include/CFXS/Base/Utility.hpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS Framework Base Module <https://github.com/CFXS/CFXS-Base>
 // Copyright (C) 2022 | CFXS / Rihards Veips
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -28,3 +28,12 @@ namespace CFXS {
     }
 
 } // namespace CFXS
+
+#define CFXS_ENUM_OPERATORS(enumName)                                                                                       \
+    constexpr enumName operator|(enumName a, enumName b) {                                                                  \
+        return static_cast<enumName>(static_cast<std::underlying_type<enumName>::type>(a) |                                 \
+                                     static_cast<std::underlying_type<enumName>::type>(b));                                 \
+    }                                                                                                                       \
+    constexpr bool operator&(enumName a, enumName b) {                                                                      \
+        return static_cast<std::underlying_type<enumName>::type>(a) & static_cast<std::underlying_type<enumName>::type>(b); \
+    }
